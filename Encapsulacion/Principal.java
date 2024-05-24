c434efe8877d11d4fabf84dd4f5e82a9155687e3
Jorge Luis Mozo castro <156432629+jorgy2910JD@users.noreply.github.com>
2024-05-23 23:00:59 -0500

    Add files via upload

---
+++ Encapsulacion/Principal.java
@@ -0,0 +1,12 @@
+
+package Encapsulacion;
+
+public class Principal {
+   public class Main {
+    public static void main(String[] args) {
+        Cafetera miCafetera = new Cafetera();
+        miCafetera.llenarAgua(500);
+        miCafetera.prepararCafe();
+        }   
+  }
+}
