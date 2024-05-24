e79b823448a732cd359b2b8b53c58d9b117fccc2
Jorge Luis Mozo castro <156432629+jorgy2910JD@users.noreply.github.com>
2024-05-24 08:04:58 -0500

    Add files via upload

---
+++ Sobrecarga/com/mycompany/Herencia/PadreRicoPadrePobre.java
@@ -0,0 +1,12 @@
+
+package Sobrecarga.com.mycompany.Herencia;
+class PadreRicoPadrePobre extends Libro {
+    String mensaje;
+    public PadreRicoPadrePobre(String titulo, String autor, String mensaje) {
+        super(titulo, autor);
+        this.mensaje = mensaje;
+    }
+}
+
+
+
---
+++ Sobrecarga/com/mycompany/Herencia/Principal.java
@@ -0,0 +1,10 @@
+
+package Sobrecarga.com.mycompany.Herencia;
+
+public class Principal {
+  PadreRicoPadrePobre libroPadreRicoPadrePobre = new PadreRicoPadrePobre("Padre Rico, Padre Pobre", "Robert T. Kiyosaki", "Mensaje inspirador sobre finanzas personales y educación financiera.");
+
+
+
+
+}
